0f79740393bbe7988e3b04013943ff5d362c5802
Thomas Cook <c3349919@uon.edu.au>
2020-02-25 14:30:54 +1100

    Inotal commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int k;
+    float x;
+    float y;
+    printf("Enter a number: ");
+    scanf("%f", &x); //y=k
+    y=9*x+32;
+    printf("y: %f\n", y); // Hello
+    return 0;
+}
